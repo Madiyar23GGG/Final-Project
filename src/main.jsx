d28a9812cd2013960570548e797007dcda35fe67
Madiyar23GGG <madiko23022006@gmail.com>
2025-07-10 13:16:52 +0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,60 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import Register from './Register'
-// import App from './folder/App'
-// import './Register.css'
-// import './Test/Login.css'
-// import Regest from './Test/Regest'
-
-
-// import Imge from './Imge'
-// import './imge.css'
-// import App2 from './UseReducer/App2'
-
-// import Homework from './Homework'
-
-// import LangApp from './UseReducer and usecontext/LangApp';
-// import LanguageContext from './UseReducer and usecontext/LanguageContext';
-// import LanguageSwitcher from './UseReducer and usecontext/LanguageSwitcher';
-
-// import Homework from './UseReducer and usecontext/Homework';
-// import './UseReducer and usecontext/style.css'
-
-
-// import Axios1 from './axios/Booklist';
-
-// import Booklist from './axios/Booklist'
-// import useSearchFilter from './axios/useSearchFilter';
-// import shop from './aaxios/shop'
-
-
-// import Apple from './Opshi project/Apple'
-// import './Opshi project/Style.css'
-
-
-// import Home from './axios/Home'
-// import Home2 from './axios/Home2'
-// import './axios/Homstyl.css'
-// import Tailwind from './Tailwind/Tailwind'
-
-// import Part from './Tailwind/Part'
-
-
-
 import Api from './Ai/Api'
-// import Toop1 from './Toop1'
-
-// import App from './Medreyting/App'
-
-// import qay from './qay'
-// import Qaitalay from './qaitalay'
-
-
-// import App from './App'
-
-
-
-
 createRoot(document.getElementById('root')).render(
     <StrictMode>
 <Api/>
